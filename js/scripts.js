6ff372b9c7ebd47c18b3972e62b33ca97712171e
theanjalimaurya <80054063+theanjalimaurya@users.noreply.github.com>
2021-03-22 17:05:39 +0100

    css update

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 console.log("Hello world");
 function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
+  var x = document.getElementById("_navlinks");
+  if (x.className === "navlinks") {
     x.className += " responsive";
   } else {
-    x.className = "navtoggle";
+    x.className = "navlinks";
   }
 }
